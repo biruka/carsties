988920903aed982373db69b40bbe3a9377305c54
Biruk <biras7070@gmail.com>
2023-08-28 23:43:31 +0300

    End of section 2

---
+++ src/AuctionService/Data/Migrations/20230828135815_attributenamechange.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class attributenamechange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Milage",
+                table: "Items",
+                newName: "Mileage");
+
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "Auctions",
+                newName: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Mileage",
+                table: "Items",
+                newName: "Milage");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Auctions",
+                newName: "status");
+        }
+    }
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Immutable;
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDBContext>(opt=>{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e){
+    Console.WriteLine(e.Message);
+}
+
+app.Run();
